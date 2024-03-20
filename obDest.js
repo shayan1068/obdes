cff9e5634887ddea03664e42732d4e5ab545a220
shayan1068 <160053138+shayan1068@users.noreply.github.com>
2024-03-21 00:26:47 +0500

    Add files via upload

---
+++ obDest.js
@@ -0,0 +1,108 @@
+//Object Destructuring 1
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); // 8
+console.log(yearNeptuneDiscovered); // 1846
+
+//Object Destructuring 2
+let planetFacts = {
+    numPlanets: 8,
+    yearNeptuneDiscovered: 1846,
+    yearMarsDiscovered: 1659
+  };
+  
+  let {numPlanets, ...discoveryYears} = planetFacts;
+  
+  console.log(discoveryYears);
+  // {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
+  //Object Destructuring 3
+  function getUserData({firstName, favoriteColor="green"}){
+    return `Your name is ${firstName} and you like ${favoriteColor}`;
+  }
+  
+  getUserData({firstName: "Alejandro", favoriteColor: "purple"})
+  // Your name is Alejandro and you like purple
+  
+  getUserData({firstName: "Melissa"})
+  // Your name is Melissa and you like green
+  
+  getUserData({})
+  // Your name is undefined and you like green
+
+  //Array Destructuring 1
+  let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+console.log(first); // Maya
+console.log(second); // Marisa
+console.log(third); // Chi
+
+//Array Destructuring 2
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+    "Raindrops on roses",
+    "whiskers on kittens",
+    "Bright copper kettles",
+    "warm woolen mittens",
+    "Brown paper packages tied up with strings"
+  ]
+  
+  console.log(raindrops); // "Raindrops on roses"
+  console.log(whiskers); // "whiskers on kittens"
+  console.log(aFewOfMyFavoriteThings);
+  // ["Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
+
+  //Array Destructuring 3
+  let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]]
+
+console.log(numbers) // [10,30,20]
+
+//ES5 Assigning Variables to Object Properties
+var obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+  
+  var a = obj.numbers.a;
+  var b = obj.numbers.b;
+
+  //ES2015 Object Destructuring
+  /* Write an ES2015 Version */
+
+const obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+  
+  const {a,b} = obj.numbers
+
+  //ES5 Array Swap
+  var arr = [1, 2];
+var temp = arr[0];
+arr[0] = arr[1];
+arr[1] = temp;
+
+//ES2015 One-Line Array Swap with Destructuring
+[arr[0], arr[1]] = [arr[1], arr[0]]
+
+
+//raceResults()
+//- first: the first element in the array
+//- second: the second element in the array
+//- third: the third element in the array
+//- rest: all other elements in the array
+//***one line* function to make this work using**
+
+// An arrow function
+//- Destructuring
+//- `Enhanced` object assignment (same key/value shortcut)
+raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre'])
+
+// {first: "Tom", second: "Margaret", third: "Allison", rest: ["David", "Pierre"]}
+
+const raceResults = ([first, second, third, ...rest]) => ({first, second, third, rest})
